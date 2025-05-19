8da37771370f8562633fcef9a0bfc75fb647bf88
monika-chandiwal <monikachandiwal04@gmail.com>
2025-05-19 23:47:57 +0530

    generate random otp

--- src/main/java/com/backend/vender/service/OtpServiceImp.java
+++ src/main/java/com/backend/vender/service/OtpServiceImp.java
@@ -1,11 +1,14 @@
 package com.backend.vender.service;
 
+import java.util.Random;
+
 public class OtpServiceImp implements OtpService{
 
     @Override
     public String generateOtp(String phoneNumber) {
         System.out.println("generate otp");
-        String otp=String.format("%06d",Integer.parseInt(String.valueOf(Math.random())));
+        Random random = new Random();
+        String otp = String.format("%06d", random.nextInt(1000000));
         return otp;
     }
 }
